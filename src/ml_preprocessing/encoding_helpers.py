5f5d6319bc3919845810a25cae16e6bd0088fe02
Mateusz_Jasieczko <mateusz.jasieczko@pmi.com>
2019-11-20 15:02:10 +0100

    encoding helpers now is GridSearch class adjusted to LOO encoding.

--- src/ml_preprocessing/encoding_helpers.py
+++ src/ml_preprocessing/encoding_helpers.py
@@ -1,158 +1,209 @@
 import itertools
-from typing import Tuple, List, DefaultDict
+from typing import List, DefaultDict, Tuple
 
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import recall_score, accuracy_score, confusion_matrix
 from sklearn.model_selection import KFold
 
 from .categorical_encoders import LeaveOneOutEncoder
 
 
-def loo_grid_search_cv(train_df,
-                       model: str,
-                       params_grid: DefaultDict,
-                       columns_to_encode: List,
-                       columns_to_drop_from_training: List,
-                       email_ohe_names: DefaultDict = None,
-                       mean: int = 1,
-                       std: int = 0.05,
-                       n_folds: int = 5) -> Tuple[pd.DataFrame, List, List, List, List]:
+class LOOGridSearchCV:
     """
-    Specially prepared function to do grid search with cross-validation on our loo encoded
+    Specially prepared class to do grid search with cross-validation on our loo encoded
     DataFrame.
 
     Scores should be approximately ok, although i have no proof for that :)
-
-    :param train_df: train_df (will be splitted then to train/and_val n_folds times)
-    :param model: model to train, currently supports only LogisticRegression
-                  and RandomForestClassifier
-    :param params_grid: param_grid to search
-    :param columns_to_encode: categorical columns, which you want to encode using loo
-    :param columns_to_drop_from_training: columns to drop from training phase
-    :param email_ohe_names: Dictionary containing names for new columns of One Hot Encoded emails
-    :param mean: mean to regularization part of the encoding
-    :param std: std to regularization part of the encoding
-    :param n_folds: n_folds to validate
-    :return: mean encoded DataFrame, scores for each model: accuracy, parameters, recalls,
-             confusion matrices
     """
 
-    processed_train_df = train_df.copy(deep=True)
+    def __init__(self,
+                 train_df: pd.DataFrame,
+                 model,
+                 params_grid: DefaultDict,
+                 columns_to_encode: List,
+                 columns_to_drop_from_training: List,
+                 Xs_train: List[pd.DataFrame] = None,
+                 ys_train: List[pd.DataFrame] = None,
+                 Xs_val: List[pd.DataFrame] = None,
+                 ys_val: List[pd.DataFrame] = None,
+                 ohe_emails: bool = True,
+                 mean: int = 1,
+                 std: int = 0.05,
+                 n_folds: int = 5,
+                 encoded_df: pd.DataFrame = pd.DataFrame(),
+                 ) -> None:
+        """
+        :param train_df: train_df (will be splitted then to train/and_val n_folds times)
+        :param model: model to train
+        :param params_grid: param_grid to search
+        :param columns_to_encode: categorical columns, which you want to encode using loo
+        :param columns_to_drop_from_training: columns to drop from training phase
+        :param ohe_emails: if set to True, performs OHE on emails column
+        :param Xs_train:
+        :param mean: mean to regularization part of the encoding
+        :param std: std to regularization part of the encoding
+        :param n_folds: n_folds to validate
+        :param encoded_df: if task was done before, just pass here already encoded_df
+        "
+        """
 
-    X = processed_train_df.reset_index().drop(columns='name')
-    y = (processed_train_df[['target']]
-         .reset_index()
-         .drop(columns='name'))
+        self.processed_train_df = train_df.copy(deep=True)
+        self.model = model
+        self.params_grid = params_grid
+        self.columns_to_encode = columns_to_encode
+        self.columns_to_drop_from_training = columns_to_drop_from_training
+        self.ohe_emails = ohe_emails
+        self.mean = mean
+        self.std = std
+        self.n_folds = n_folds
+        if not Xs_train:
+            self.Xs_train, self.ys_train, self.Xs_val, self.ys_val = ([] for i in range(4))
+        else:
+            self.Xs_train = Xs_train
+            self.ys_train = ys_train
+            self.Xs_val = Xs_val
+            self.ys_val = ys_val
+        self.encoded_df_ = encoded_df
 
-    """
-    one hot encoding of email column
-    """
-    if email_ohe_names:
-        processed_train_df = (pd.concat([processed_train_df,
-                                         pd.get_dummies(processed_train_df['emails'])], axis=1)
-                                .rename(columns=email_ohe_names))
-        columns_to_drop_from_training.append('emails')
+    def _ohe_emails(self) -> pd.DataFrame:
+        """
+        internal method for one hot encoding emails column
+        """
+        email_ohe_names = {0: '0_emails',
+                           1: '1_email',
+                           2: '2_emails',
+                           3: '3_emails',
+                           4: '4_emails',
+                           5: '5_emails'}
 
-    X.drop(columns=columns_to_drop_from_training, inplace=True)
+        self.processed_train_df = (pd.concat([self.processed_train_df, pd.get_dummies(
+            self.processed_train_df['emails'])], axis=1)
+                                   .rename(columns=email_ohe_names))
+        self.columns_to_drop_from_training.append('emails')
 
-    """
-    what we do here is preparing training/validation datasets, then encoding them, then we
-    pass this information further to appropriate modelling part
-    (we do the split here, because it is sufficient to do categorical encoding once here, than
-    doing it over and over in the modelling part)
-    """
-    kf = KFold(n_splits=n_folds, shuffle=False, random_state=None)
-    """
-    to have each sample exactly once in validation set
-    """
+        return self.processed_train_df.copy(deep=True)
 
-    splits = kf.split(processed_train_df)
+    def _prepare_train_val_dfs(self):
+        """
+        Internal method
 
-    dfs_to_mean = []
-    Xs_train, ys_train, Xs_val, ys_val = ([] for i in range(4))
-    for train_index, val_index in splits:
-        X_train, y_train = X.iloc[train_index], y.iloc[train_index]
-        X_val, y_val = X.iloc[val_index], y.iloc[val_index]
-        X_val.drop(columns=['target'], inplace=True)
+        bunch of code to prepare train and validation dataframes for given n_folds, to make
+        grid search and cross validation processes much faster: for each n_folds you will need
+        to compute encoded_df only once, same for validation and train DataFrames
+        """
 
-        enc = LeaveOneOutEncoder(train_df=X_train,
-                                 test_df=X_val,
-                                 columns_to_encode=columns_to_encode,
-                                 target_column='target',
-                                 random_state=42,
-                                 mean=mean,
-                                 std=std)
-        X_train, X_val = enc.fit()
-        encoded_cols = [col for col in X_train.columns if 'encoded_' in col]
-        dfs_to_mean.append(X_train[encoded_cols])
-        train_to_drop = columns_to_encode.copy()
-        train_to_drop.extend(['target'])
-        X_train.drop(columns=train_to_drop, inplace=True)
-        test_to_drop = columns_to_encode.copy()
-        X_val.drop(columns=test_to_drop, inplace=True)
+        X = self.processed_train_df.reset_index().drop(columns='name')
+        y = self.processed_train_df[['target']].reset_index().drop(columns='name')
 
-        Xs_train.append(X_train)
-        ys_train.append(y_train)
-        Xs_val.append(X_val)
-        ys_val.append(y_val)
+        if self.ohe_emails:
+            self.processed_train_df = self._ohe_emails()
 
-    """
-    we are computing here the mean of the folds with excluding the 'i am now validation not the 
-    training set' part, as I see it as the most proper thing to do, to use cross-validation 
-    approach
-    """
-    for df in dfs_to_mean:
-        zeros = [0 for col in df.columns]
-        for index in range(len(processed_train_df)):
-            if index not in df.index:
-                df.loc[index, :] = zeros
-        df.sort_index(inplace=True)
-
-    mean_df = dfs_to_mean[0].copy(deep=True)
-    mean_df = mean_df * 0
-    for num in range(n_folds):
-        mean_df = mean_df + dfs_to_mean[num]
-    encoded_final_df = mean_df.divide(n_folds - 1)
-
-    """
-    GridSearch right here
-    """
-    models_accuracies, models_recalls, models_parameters, models_cms = ([] for i in range(4))
-    for p in itertools.product(*params_grid.values()):
-        model_params = params_grid.copy()
-        for counter, key in enumerate(model_params.keys()):
-            model_params[key] = p[counter]
-
-        models_parameters.append(model_params.items())
-        if model == 'LogisticRegression':
-            clf = LogisticRegression(**model_params)
-
-        if model == 'RandomForestClassifier':
-            clf = RandomForestClassifier(**model_params)
-
-        cv_accuracies, cv_recalls, cv_cms = ([] for i in range(3))
+        X.drop(columns=self.columns_to_drop_from_training, inplace=True)
 
         """
-        fitting and predicting for all folds, then scoring them by: 
-        accuracy, recall and confusion matrix
+        to have each sample exactly once in validation set
         """
-        for index in range(n_folds):
-            clf.fit(Xs_train[index], ys_train[index])
-            predictions = clf.predict(Xs_val[index])
-            cv_accuracies.append(accuracy_score(ys_val[index], predictions))
-            cv_recalls.append(recall_score(ys_val[index], predictions))
-            cv_cms.append(confusion_matrix(ys_val[index], predictions))
+        kf = KFold(n_splits=self.n_folds, shuffle=False, random_state=None)
+        splits = kf.split(self.processed_train_df)
+
+        dfs_to_mean = []
+        for train_index, val_index in splits:
+            X_train, y_train = X.iloc[train_index], y.iloc[train_index]
+            X_val, y_val = X.iloc[val_index], y.iloc[val_index]
+            X_val.drop(columns=['target'], inplace=True)
+
+            enc = LeaveOneOutEncoder(train_df=X_train,
+                                     test_df=X_val,
+                                     columns_to_encode=self.columns_to_encode,
+                                     target_column='target',
+                                     random_state=42,
+                                     mean=self.mean,
+                                     std=self.std)
+
+            X_train, X_val = enc.fit()
+            encoded_cols = [col for col in X_train.columns if 'encoded_' in col]
+            dfs_to_mean.append(X_train[encoded_cols])
+            train_to_drop = self.columns_to_encode.copy()
+            train_to_drop.extend(['target'])
+            X_train.drop(columns=train_to_drop, inplace=True)
+            test_to_drop = self.columns_to_encode.copy()
+            X_val.drop(columns=test_to_drop, inplace=True)
+
+            self.Xs_train.append(X_train)
+            self.ys_train.append(y_train)
+            self.Xs_val.append(X_val)
+            self.ys_val.append(y_val)
 
         """
-        final evaluation of scores (means of all folds scores
-        for confusion matrix we can get not integer values, please treat this more informative
-        than strict - but anyway, as a source of information which model should we choose
+        we are computing here the mean of the folds with excluding the 'i am now validation not the 
+        training set' part, as I see it as the most proper thing to do, to use cross-validation 
+        approach
         """
-        models_accuracies.append(np.mean(cv_accuracies))
-        models_recalls.append(np.mean(cv_recalls))
-        models_cms.append(np.mean(cv_cms, axis=0))
+        for df in dfs_to_mean:
+            zeros = [0 for col in df.columns]
+            for index in range(len(self.processed_train_df)):
+                if index not in df.index:
+                    df.loc[index, :] = zeros
+            df.sort_index(inplace=True)
 
-    return encoded_final_df, models_accuracies, models_parameters, models_recalls, models_cms
+        mean_df = dfs_to_mean[0].copy(deep=True)
+        mean_df = mean_df * 0
+        for num in range(self.n_folds):
+            mean_df = mean_df + dfs_to_mean[num]
+        self.encoded_df_ = mean_df.divide(self.n_folds - 1)
+
+    def grid_search(self) -> Tuple[List, List, List, List]:
+        """
+        performs GridSearchCV
+
+        :return: list with each of the models: accuracies, parameters, recalls and confusion
+        matrices
+        """
+        if self.encoded_df_.empty:
+            self._prepare_train_val_dfs()
+
+        models_accuracies, models_recalls, models_parameters, models_cms = ([] for i in range(4))
+        for p in itertools.product(*self.params_grid.values()):
+            model_params = self.params_grid.copy()
+            for counter, key in enumerate(model_params.keys()):
+                model_params[key] = p[counter]
+
+            models_parameters.append(model_params.items())
+            clf = self.model.set_params(**model_params)
+
+            cv_accuracies, cv_recalls, cv_cms = ([] for i in range(3))
+
+            """
+            fitting and predicting for all folds, then scoring them by: 
+            accuracy, recall and confusion matrix
+            """
+            for index in range(self.n_folds):
+                clf.fit(self.Xs_train[index], self.ys_train[index])
+                predictions = clf.predict(self.Xs_val[index])
+                cv_accuracies.append(accuracy_score(self.ys_val[index], predictions))
+                cv_recalls.append(recall_score(self.ys_val[index], predictions))
+                cv_cms.append(confusion_matrix(self.ys_val[index], predictions))
+
+            """
+            final evaluation of scores (means of all folds scores
+            for confusion matrix we can get not integer values, please treat this more informative
+            than strict - but anyway, as a source of information which model should we choose
+            """
+            models_accuracies.append(np.mean(cv_accuracies))
+            models_recalls.append(np.mean(cv_recalls))
+            models_cms.append(np.mean(cv_cms, axis=0))
+
+        return models_accuracies, models_parameters, models_recalls, models_cms
+
+    def processed_train(self):
+        """
+        :return: processed train DataFrame with added encoded columns
+        """
+        train = self.processed_train_df.copy(deep=True)
+        encoded = self.encoded_df_.copy(deep=True)
+        train = train.reset_index().drop(columns=['name'])
+        train = train.drop(columns=self.columns_to_drop_from_training+self.columns_to_encode)
+        processed_train = pd.concat([train, encoded], axis=1)
+
+        return processed_train
